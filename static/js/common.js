d082033cc18591c8b8d48b23ff7caf32f0d20a38
cuihuan <cuihuan@didiglobal.com>
2020-02-03 19:49:10 +0800

    核心数据居中显示

--- static/js/common.js
+++ static/js/common.js
@@ -265,3 +265,10 @@ function checkPrcId(prcId) {
     return !!pattern.test(prcId);
 }
 
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?15d3caeed62b266d96dd734894786f13";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
